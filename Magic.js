9f14335e96add8874614d1812c2d0562885fa270
SilverNeko21 <larong2122@gmail.com>
2017-11-18 21:51:05 -1000

    done

--- Magic.js
+++ Magic.js
@@ -1,5 +1,9 @@
+//used in the function and a means to console.log my function.
 var output ;
-
+//sentence to show when the if statement's conditions are not met.
+var p = "Please enter either add, subtract, multiply, or divide for x and 2 numbers for y and z.";
+ 
+ //function with if statement
 function calculator(x, y, z) {
  if (x == "add"){
     output = y + z;
@@ -9,8 +13,12 @@ function calculator(x, y, z) {
     output = y * z;
 } else if (x == "divide"){
     output = y / z;
-} 
+} else {
+	output = p;
+}
 }
 
-calculator("multiply", 5, 5);
+//inputs for x, y, and z.
+calculator( "multiply", 10, 5);
 console.log(output);
+
